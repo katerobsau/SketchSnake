acab540a60e5a50da0647464efc51da719261bf4
Kate Saunders <katerobinsonsaunders@gmail.com>
2021-04-03 11:42:44 +1000

    Code to alter snake direction
    * alters direction with head of snake
    * small issue with boolean direction check that needs a fix

--- snake_direction.R
+++ snake_direction.R
@@ -16,11 +16,12 @@ draw <- function(){
   snake_col = color('rgb(0,255,0)') 
   snake_wid =  10
   start_snake_len = 5
-  snake_speed = 200
-  
+    
   # Initialise snake
   if(frameCount < 2){
     snake_len = start_snake_len
+    # old_snake_x = seq(0, start_snake_len)
+    # old_snake_y = seq(0, start_snake_len)
     snake_x = seq(0,start_snake_len)*snake_wid + bkgrd_x/2
     snake_y = seq(0,start_snake_len)*0 + bkgrd_y/2
   }
@@ -35,49 +36,75 @@ draw <- function(){
   if(frameCount > 2){
     
     # Plain right shift
-    ### NOT WORKING
     if(keyCode == RIGHT_ARROW){
+      direction = "right"
       old_snake_x = snake_x
       old_snake_y = snake_y
-      for(i in 1:snake_len){
-        snake_x[i] = old_snake_x[i-1]
-        snake_y[i] = old_snake_y[i-1]
-        if(snake_x[i] > bkgrd_x){
-          snake_x[i] = 0
-        } 
+      for(i in 0:(snake_len - 1)){
+        snake_x[i] = old_snake_x[i+1]
+        snake_y[i] = old_snake_y[i+1]
       }
-      snake_x[snake_len] = old_snake_x[snake_len] + snake_wid
+      new_x = old_snake_x[snake_len] + snake_wid
+      snake_x[snake_len] = new_x
+      if(new_x > bkgrd_x) snake_x[snake_len] = 0
     }
     
-    # # Plain left shift
-    # if(keyCode == LEFT_ARROW){
-    #   snake_x = snake_x - snake_wid
-    #   for(i in 0:snake_len){
-    #     if(snake_x[i] < 0){
-    #       snake_x[i] = bkgrd_x
-    #     }
-    #   }
-    # }
-    # 
-    # # Downwards shift
-    # if(keyCode == DOWN_ARROW){
-    #   snake_y = snake_y + snake_wid
-    #   for(i in 0:snake_len){
-    #     if(snake_y[i] > bkgrd_y){
-    #       snake_y[i] = 0
-    #     }
-    #   }
-    # }
-    # 
-    # # Upwards shift
-    # if(keyCode == UP_ARROW){
-    #   snake_y = snake_y - snake_wid
-    #   for(i in 0:snake_len){
-    #     if(snake_y[i] < 0){
-    #       snake_y[i] = bkgrd_y
-    #     }
-    #   }
-    # }
+    # Plain left shift
+    if(keyCode == LEFT_ARROW){
+      
+      old_snake_x = snake_x
+      old_snake_y = snake_y
+      
+      # if(direction == "right"){
+        for(i in 1:snake_len){
+          snake_x[i] = old_snake_x[i-1]
+          snake_y[i] = old_snake_y[i-1]
+        }
+        new_x = old_snake_x[0] - snake_wid
+        snake_x[0] = new_x
+        if(new_x < 0) snake_x[0] = bkgrd_x
+      # }
+      
+      # if(direction != "right"){
+      #   for(i in 0:(snake_len - 1)){
+      #     snake_x[i] = old_snake_x[i+1]
+      #     snake_y[i] = old_snake_y[i+1]
+      #   }
+      #   # new_x = old_snake_x[snake_len] - snake_wid
+      #   # snake_x[snake_len] = new_x
+      #   # if(new_x < 0) snake_x[snake_len] = bkgrd_x
+      # }
+      
+      direction = "left"
+    }
+    
+    # Downwards shift
+    if(keyCode == DOWN_ARROW){
+      direction = "down"
+      old_snake_x = snake_x
+      old_snake_y = snake_y
+      for(i in 0:(snake_len - 1)){
+        snake_x[i] = old_snake_x[i+1]
+        snake_y[i] = old_snake_y[i+1]
+      }
+      new_y = old_snake_y[snake_len] + snake_wid
+      snake_y[snake_len] = new_y
+      if(new_y > bkgrd_y) snake_y[snake_len] = 0
+    }
+    
+    # Upwards shift
+    if(keyCode == UP_ARROW){
+      direction = "up"
+      old_snake_x = snake_x
+      old_snake_y = snake_y
+      for(i in 0:(snake_len - 1)){
+        snake_x[i] = old_snake_x[i+1]
+        snake_y[i] = old_snake_y[i+1]
+      }
+      new_y = old_snake_y[snake_len] - snake_wid
+      snake_y[snake_len] = new_y
+      if(new_y < 0) snake_y[snake_len] = bkgrd_y
+    }
     
   }
 
